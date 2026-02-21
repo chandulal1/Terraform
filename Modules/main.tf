9624123335f8cdb6a2c809e2ab75c55a2e55dbfb
chandulal1 <83697943+chandulal1@users.noreply.github.com>
2026-02-22 01:20:57 +0530

    Add VPC, subnet, security group, and S3 modules

--- Modules/main.tf
+++ Modules/main.tf
@@ -2,9 +2,36 @@ provider "aws" {
   region = "us-east-1"
 }
 
-module "ec2_instance" {
+module "vpc" {
+  source     = "./modules/vpc"
+  cidr_block = "10.0.0.0/16"
+  vpc_name   = "phase3-vpc"
+}
+
+module "subnet" {
+  source      = "./modules/subnet"
+  vpc_id      = module.vpc.vpc_id
+  cidr_block  = "10.0.1.0/24"
+  az          = "us-east-1a"
+  subnet_name = "phase3-subnet"
+}
+
+module "sg" {
+  source  = "./modules/security-group"
+  vpc_id  = module.vpc.vpc_id
+  sg_name = "phase3-sg"
+}
+
+module "ec2" {
   source         = "./modules/ec2"
-  ami_id         = "ami-xxxxxxxx"   # replace with valid AMI
+  ami_id         = "ami-xxxxxxxx"  # replace
   instance_type  = "t2.micro"
-  instance_name  = "phase3-module"
+  subnet_id      = module.subnet.subnet_id
+  sg_id          = module.sg.sg_id
+  instance_name  = "phase3-ec2"
+}
+
+module "s3" {
+  source      = "./modules/s3"
+  bucket_name = "phase3-unique-bucket-name-123"
 }
