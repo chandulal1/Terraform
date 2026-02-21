eab828d57e8e48490c422d6b34f38114cb07cff9
chandulal1 <83697943+chandulal1@users.noreply.github.com>
2026-02-22 01:18:02 +0530

    Add subnet and security group to EC2 instance

--- Modules/EC2/main.tf
+++ Modules/EC2/main.tf
@@ -1,6 +1,8 @@
 resource "aws_instance" "this" {
-  ami           = var.ami_id
-  instance_type = var.instance_type
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  subnet_id              = var.subnet_id
+  vpc_security_group_ids = [var.sg_id]
 
   tags = {
     Name = var.instance_name
