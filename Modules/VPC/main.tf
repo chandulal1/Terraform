96e1597e03961801ac4fac1566989259cae4774f
chandulal1 <83697943+chandulal1@users.noreply.github.com>
2026-02-22 01:14:27 +0530

    Add AWS subnet resource to VPC module

---
+++ Modules/VPC/main.tf
@@ -0,0 +1,9 @@
+resource "aws_subnet" "this" {
+  vpc_id            = var.vpc_id
+  cidr_block        = var.cidr_block
+  availability_zone = var.az
+
+  tags = {
+    Name = var.subnet_name
+  }
+}
