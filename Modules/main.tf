b9a6ce1c7b1aa3e1e0f4df671c9086bfd6c7952d
chandulal1 <83697943+chandulal1@users.noreply.github.com>
2026-02-22 01:28:04 +0530

    Refactor main.tf to use variables for configuration
    Refactored main.tf to use variables for region, VPC CIDR, subnet CIDR, availability zone, AMI ID, instance type, and bucket name.

--- Modules/main.tf
+++ Modules/main.tf
@@ -1,18 +1,18 @@
 provider "aws" {
-  region = "us-east-1"
+  region = var.region
 }
 
 module "vpc" {
   source     = "./modules/vpc"
-  cidr_block = "10.0.0.0/16"
+  cidr_block = var.vpc_cidr
   vpc_name   = "phase3-vpc"
 }
 
 module "subnet" {
   source      = "./modules/subnet"
   vpc_id      = module.vpc.vpc_id
-  cidr_block  = "10.0.1.0/24"
-  az          = "us-east-1a"
+  cidr_block  = var.subnet_cidr
+  az          = var.availability_zone
   subnet_name = "phase3-subnet"
 }
 
@@ -24,8 +24,8 @@ module "sg" {
 
 module "ec2" {
   source         = "./modules/ec2"
-  ami_id         = "ami-xxxxxxxx"  # replace
-  instance_type  = "t2.micro"
+  ami_id         = var.ami_id
+  instance_type  = var.instance_type
   subnet_id      = module.subnet.subnet_id
   sg_id          = module.sg.sg_id
   instance_name  = "phase3-ec2"
@@ -33,5 +33,5 @@ module "ec2" {
 
 module "s3" {
   source      = "./modules/s3"
-  bucket_name = "phase3-unique-bucket-name-123"
+  bucket_name = var.bucket_name
 }
